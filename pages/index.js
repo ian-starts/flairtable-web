59e29431ff020a4bb580c509b8940f85809b6227
Yoni Ian Kok <hello@yik.dev>
2020-04-13 19:57:27 +0200

    remove unknown deps

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import Layout from "../components/Layout";
 import WithProfile from "../hocs/WithProfile";
-import getClient from "../services/request";
 import * as authService from '../services/authService';
-import ReserveRow from "../components/ReserveCol";
-import moment from "moment";
 
 const Home = (props) => {
     const [reservations, setReservations] = useState([]);
     useEffect(() => {
-        getClient(authService.getToken()).get(`washingtime?page[size]=10000&filter[dateGte]=${moment().startOf('d').toISOString()}`).then(resp => {
-            setReservations(resp.data.data);
-        })
+
     }, []);
     return (<div>
             <Layout locale={props.locale} title="Flairtable">
